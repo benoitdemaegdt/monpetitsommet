f6f72eae2999c0a1d94fcc366debf5b811ed47e7
benoitdemaegdt <benoit.demaegdt@gmail.com>
2020-12-24 15:44:36 +0100

    design landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="pt-12 w-full h-screen mx-auto flex flex-col justify-center items-center backgound">
-    <div class="grid grid-cols-2 gap-4">
-      <div class="flex justify-center">
-        <nuxt-image src="/img/vercors.png" class="w-3/4 h-auto object-cover object-center" />
-      </div>
-      <div class="flex flex-col justify-center">
-        <h1 class="text-gray-100 text-3xl md:text-4xl lg:text-5xl font-bold">
-          Préparez votre prochaine aventure en montagne
-        </h1>
-        <p class="w-3/4 text-gray-100 text-base md:text-xl lg:text-2xl font-semibold">
-          Découvrez les topos détaillés d'aventures sportives en montagne. Parfois engagées. Souvent loin des foules. Toujours mémorables.
-        </p>
+  <div class="pt-12 w-full h-screen mx-auto flex flex-col justify-center items-center">
+    <div class="w-full relative flex flex-col justify-center" style="height: 90%;">
+      <nuxt-image src="/img/vercors.png" class="landing-image" />
+      <div class="grid grid-cols-2 gap-4 backgound" style="height: 80%;">
+        <div />
+        <div class="flex flex-col justify-center">
+          <h1 class="text-gray-100 text-3xl md:text-4xl lg:text-5xl font-bold leading-tight">
+            Préparez votre prochaine aventure en montagne
+          </h1>
+          <p class="mt-6 w-3/4 text-gray-100 text-base md:text-xl lg:text-2xl font-semibold">
+            Découvrez les topos détaillés d'aventures sportives en montagne. Parfois engagées. Souvent loin des foules. Toujours mémorables.
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -24,5 +25,13 @@ export default {
 <style scoped>
 .backgound {
   background-color: #1f374c;
+}
+
+.landing-image {
+  position: absolute !important;
+  top: 0;
+  left: 10%;
+  height: 100%;
+  width: 35%;
 }
 </style>
