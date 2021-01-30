dfd28158f8fc61e2e7bfcfa5bc005c343bb2b267
benoitdemaegdt <benoit.demaegdt@gmail.com>
2021-01-30 19:41:17 +0100

    add more sections to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="pb-24">
     <div class="pt-12 w-full h-screen mx-auto">
       <div class="h-full w-11/12 mx-auto grid grid-cols-2">
         <div class="w-5/6 mx-auto flex flex-col justify-center">
@@ -32,7 +32,7 @@
       <div class="mt-8 flex flex-wrap justify-around">
         <NuxtLink v-for="trek in treks" :key="trek.title" :to="{ name: `massifs-${trek.massif}-randonnees-slug`, params: { slug: trek.slug } }" class="px-1">
           <div class="max-w-sm bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-xl">
-            <!-- <nuxt-img class="w-full h-56" :src="trek.img" /> -->
+            <nuxt-img provider="cloudinary" :src="trek.img" class="w-full h-56 object-cover" />
             <div class="px-4 py-4">
               <div class="text-xs title-font text-gray-600 tracking-widest">
                 {{ trek.from.toUpperCase() }} &rarr; {{ trek.to.toUpperCase() }}
@@ -45,7 +45,7 @@
                 <div class="flex items-center">
                   <nuxt-img src="/icons/distance.png" class="h-5 w-5" />
                   <div class="ml-2">
-                    <span class="hidden sm:inline">Distance : </span>{{ trek.distance }} km
+                    <span class="hidden sm:inline">Distance : </span>{{ Math.round(trek.distance) }} km
                   </div>
                 </div>
                 <!-- duration -->
@@ -87,11 +87,78 @@
         En plus des refuges, on trouve de nombreuses cabanes non gardées dans les massifs français. Il s'agit la plupart du temps d'abris très sommaires, ouverts été comme hiver afin d'accueillir les randonneurs de passage. Passer une nuit dans l'une de ces cabanes est une occasion unique de vivre une expérience inoubliable en plein coeur de la nature.
       </p>
     </div>
+    <div class="mt-8 w-full px-4">
+      <div class="flex flex-wrap justify-around">
+        <NuxtLink v-for="refuge in refuges" :key="refuge.name" :to="{ name: `massifs-${refuge.massif}-refuges-slug`, params: { slug: refuge.slug } }" class="px-1">
+          <div class="max-w-sm bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-xl">
+            <nuxt-img provider="cloudinary" :src="refuge.img" class="w-full h-56 object-cover" />
+            <div class="px-4 py-4">
+              <div class="text-xs title-font text-gray-600 tracking-widest">
+                {{ refuge.type.toUpperCase() }}
+              </div>
+              <div class="text-gray-800 font-semibold text-xl mb-2">
+                {{ refuge.name }}
+              </div>
+              <div class="py-3 grid grid-cols-2 gap-4 text-sm text-gray-800">
+                <!-- altitude -->
+                <div class="flex items-center">
+                  <nuxt-img src="/icons/mountain.png" class="h-5 w-5" />
+                  <div class="ml-2">
+                    <span class="hidden sm:inline">Altitude : </span>{{ refuge.altitude }}m
+                  </div>
+                </div>
+                <!-- beds -->
+                <div class="flex items-center">
+                  <nuxt-img src="/icons/bed.png" class="h-5 w-5" />
+                  <div class="ml-2">
+                    <span class="hidden sm:inline">Capacité : </span>{{ refuge.beds }} pers
+                  </div>
+                </div>
+                <!-- stove -->
+                <div class="flex items-center">
+                  <nuxt-img src="/icons/stove.png" class="h-5 w-5" />
+                  <div class="ml-2">
+                    Poêle : {{ refuge.stove | booleanToFrench }}
+                  </div>
+                </div>
+                <!-- water -->
+                <div class="flex items-center">
+                  <nuxt-img src="/icons/water.png" class="h-5 w-5" />
+                  <div class="ml-2">
+                    Eau : {{ refuge.water | booleanToFrench }}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="px-6 pb-4">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#{{ refuge.massif }}</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#refuge</span>
+            </div>
+          </div>
+        </NuxtLink>
+      </div>
+    </div>
+    <div class="mt-16 w-3/4 mx-auto">
+      <h2 class="text-gray-800 text-4xl text-center font-semibold">
+        Découvrez vos prochaines aventures
+      </h2>
+      <p class="mt-4 text-gray-600 text-xl text-justify font-base">
+        Randonnées, bivouac, escalade, alpinisme, ski de rando et même snowkite : des aventures en montagne il y en a pour tous les goûts ! Inscrivez vous à la newsletter de Mon Petit Sommet pour recevoir directement dans votre boite mail les dernières sorties publiées sur le site.
+      </p>
+      <div class="mt-6 flex justify-center ">
+        <a href="https://monpetitsommet.substack.com/" target="_blank" rel="noopener noreferrer" class="text-white font-semibold bg-green-600 border-0 py-2 px-6 focus:outline-none hover:bg-green-700 rounded">
+          S'INSCRIRE À LA NEWSLETTER &rarr;
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import HelperMixin from '@/mixins/HelperMixin.js'
+
 export default {
+  mixins: [HelperMixin],
   async asyncData ({ $content, params }) {
     const treks = await $content('randonnees', { deep: true }, params.slug)
       .where({ title: { $in: ['Traversée du Vercors (Balcon Est)', 'Traversée des Bauges'] } })
@@ -101,9 +168,12 @@ export default {
       ])
       .fetch()
 
-    return {
-      treks
-    }
+    const refuges = await $content('refuges', { deep: true }, params.slug)
+      .where({ name: { $in: ['Cabane de Pré Peyret', 'Cabane des Aiguillettes', 'Cabane PNRV des Chaumailloux'] } })
+      .only(['massif', 'name', 'type', 'img', 'beds', 'altitude', 'stove', 'stove', 'water', 'slug'])
+      .fetch()
+
+    return { treks, refuges }
   },
   data: () => ({
     image: 'w_900/v1604305406/vercors/randonnees/traversee-vercors-balcon-est/intro_xhbx3y.png'
