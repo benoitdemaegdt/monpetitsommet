d26e4c3953b1a9be7108ce03b6138f0717bf427f
benoitdemaegdt <benoit.demaegdt@gmail.com>
2021-01-31 19:42:25 +0100

    fix responsive design on cgu page

--- pages/cgu/index.vue
+++ pages/cgu/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="pt-24 px-12 mx-auto">
-    <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">
+  <div class="py-16 px-2 md:py-24 md:px-12 mx-auto">
+    <h1 class="sm:text-3xl text-2xl font-bold title-font text-gray-900">
       Mentions légales
     </h1>
-    <h2 class="mt-2 sm:text-2xl text-xl font-medium title-font text-gray-800">
+    <h2 class="mt-2 sm:text-2xl text-xl font-semibold title-font text-gray-800">
       Hébergement
     </h2>
     <p class="mt-2 text-gray-800">
-      Ce site web est hébergé par <a href="https://www.netlify.com/" target="_blank" rel="noopener noreferrer">Netlify</a>
+      Ce site web est hébergé par <a href="https://www.netlify.com/" target="_blank" rel="noopener noreferrer" class="text-green-700 hover:underline">Netlify</a>
     </p>
-    <h2 class="mt-2 sm:text-2xl text-xl font-medium title-font text-gray-800">
+    <h2 class="mt-2 sm:text-2xl text-xl font-semibold title-font text-gray-800">
       Protection des données à caractère personnel
     </h2>
     <p class="mt-2 text-gray-800">
@@ -20,7 +20,7 @@
         <li>Pas d’usage des informations collectées au-delà du traitement de la demande.</li>
       </ul>
     </p>
-    <h2 class="mt-2 sm:text-2xl text-xl font-medium title-font text-gray-800">
+    <h2 class="mt-2 sm:text-2xl text-xl font-semibold title-font text-gray-800">
       Limitation de responsabilité
     </h2>
     <p class="mt-2 text-gray-800">
