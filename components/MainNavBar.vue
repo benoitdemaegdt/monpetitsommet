e38aa3da5450ee67cf659f7010ffea81795ad8ae
benoitdemaegdt <benoit.demaegdt@gmail.com>
2021-04-24 22:02:03 +0200

    add template for belledonne & chartreuse

--- components/MainNavBar.vue
+++ components/MainNavBar.vue
@@ -96,8 +96,15 @@ export default {
         activities: [
           { name: 'Randonnées', key: 'randonnees', link: 'massifs-chartreuse-randonnees', icon: 'M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7' }
         ]
+      },
+      {
+        name: 'Belledonne',
+        key: 'belledonne',
+        activities: [
+          { name: 'Randonnées', key: 'randonnees', link: 'massifs-belledonne-randonnees', icon: 'M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7' }
+        ]
       }
-    ]
+    ].sort((a, b) => a.key > b.key ? 1 : -1)
   }),
   methods: {
     toggleDrawer () {
---
+++ pages/massifs/chartreuse/randonnees/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container px-5 pt-12 mx-auto">
+    <div class="mt-4 flex justify-center">
+      <h1 class="text-2xl md:text-3xl lg:text-4xl text-gray-900 font-semibold">
+        Randonner en chartreuse
+      </h1>
+    </div>
+    <div class="mt-2 mx-auto">
+      <p class="text-base md:text-lg text-gray-700 text-justify">
+        Coming soon ...
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // async asyncData ({ $content, params }) {
+  //   const treks = await $content('randonnees/chartreuse', params.slug)
+  //     .only([
+  //       'title', 'description', 'img', 'rating', 'distance', 'elevation',
+  //       'duration', 'difficulty', 'from', 'to', 'slug', 'createdAt'
+  //     ])
+  //     .fetch()
+
+  //   return {
+  //     treks
+  //   }
+  // }
+}
+</script>
