e7bc3976da1072738bda61fa8c6c1ca081108a01
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-01-08 13:07:18 +0100

    config eslint x prettier

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,6 @@ import getRoutes from './utils/getRoutes'
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-  generate: { fallback: true },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -15,7 +14,8 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: "Découvrez les topos détaillés d'aventures sportives en montagne. Parfois engagées. Souvent loin des foules. Toujours mémorables."
+        content:
+          "Découvrez les topos détaillés d'aventures sportives en montagne. Parfois engagées. Souvent loin des foules. Toujours mémorables.",
       },
       // facebook
       { property: 'og:site_name', content: 'Mon Petit Sommet' },
@@ -25,12 +25,14 @@ export default {
       {
         hid: 'og:description',
         property: 'og:description',
-        content: "Découvrez les topos détaillés d'aventures sportives en montagne. Parfois engagées. Souvent loin des foules. Toujours mémorables."
+        content:
+          "Découvrez les topos détaillés d'aventures sportives en montagne. Parfois engagées. Souvent loin des foules. Toujours mémorables.",
       },
       {
         hid: 'og:image',
         property: 'og:image',
-        content: 'https://res.cloudinary.com/monpetitsommet/image/upload/v1604305406/vercors/randonnees/traversee-vercors-balcon-est/intro_xhbx3y.png'
+        content:
+          'https://res.cloudinary.com/monpetitsommet/image/upload/v1604305406/vercors/randonnees/traversee-vercors-balcon-est/intro_xhbx3y.png',
       },
       { property: 'og:image:width', content: '740' },
       { property: 'og:image:height', content: '300' },
@@ -41,27 +43,27 @@ export default {
       {
         hid: 'twitter:description',
         name: 'twitter:description',
-        content: "Découvrez les topos détaillés d'aventures sportives en montagne. Parfois engagées. Souvent loin des foules. Toujours mémorables."
+        content:
+          "Découvrez les topos détaillés d'aventures sportives en montagne. Parfois engagées. Souvent loin des foules. Toujours mémorables.",
       },
       {
         hid: 'twitter:image',
         name: 'twitter:image',
-        content: 'https://res.cloudinary.com/monpetitsommet/image/upload/v1604305406/vercors/randonnees/traversee-vercors-balcon-est/intro_xhbx3y.png'
-      }
+        content:
+          'https://res.cloudinary.com/monpetitsommet/image/upload/v1604305406/vercors/randonnees/traversee-vercors-balcon-est/intro_xhbx3y.png',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-      { hid: 'canonical', rel: 'canonical', href: 'https://monpetitsommet.fr' }
-    ]
+      { hid: 'canonical', rel: 'canonical', href: 'https://monpetitsommet.fr' },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -72,14 +74,14 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
-    '@nuxt/image'
+    '@nuxt/image',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
-    '@nuxtjs/sitemap'
+    '@nuxtjs/sitemap',
   ],
 
   // Content module configuration (https://go.nuxtjs.dev/config-content)
@@ -91,26 +93,27 @@ export default {
   // Sitemap
   sitemap: {
     hostname: 'https://monpetitsommet.fr/',
-    routes () {
+    routes() {
       return getRoutes()
-    }
+    },
   },
 
   // Image
   image: {
     sizes: [320, 420, 768, 1024],
     cloudinary: {
-      baseURL: 'https://res.cloudinary.com/monpetitsommet/image/upload/'
-    }
+      baseURL: 'https://res.cloudinary.com/monpetitsommet/image/upload/',
+    },
   },
 
   // Tailwind
   tailwindcss: {
-    jit: true
+    jit: true,
   },
 
   // env
   env: {
-    NUXT_ENV_MAPBOX_GL_TOKEN: 'pk.eyJ1IjoiYmVub2l0ZGVtYWVnZHQiLCJhIjoiY2s5dnBwaWV5MDFmdjNmazZ1dGFjNDBoYyJ9.0LPj3EUfQdPOQh-S41Sn4A'
-  }
+    NUXT_ENV_MAPBOX_GL_TOKEN:
+      'pk.eyJ1IjoiYmVub2l0ZGVtYWVnZHQiLCJhIjoiY2s5dnBwaWV5MDFmdjNmazZ1dGFjNDBoYyJ9.0LPj3EUfQdPOQh-S41Sn4A',
+  },
 }
