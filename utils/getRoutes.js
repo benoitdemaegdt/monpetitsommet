3179c67b57a695082b26eee36539d3d6e9cf1639
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-01-08 20:34:18 +0100

    update sitemap.xml

--- utils/getRoutes.js
+++ utils/getRoutes.js
@@ -1,12 +1,27 @@
 export default async () => {
   const { $content } = require('@nuxt/content')
-  const articles = await $content('articles', { deep: true }).only(['slug']).fetch()
-  const randonnees = await $content('randonnees', { deep: true }).only(['massif', 'slug']).fetch()
-  const refuges = await $content('refuges', { deep: true }).only(['massif', 'slug']).fetch()
+  const articles = await $content('articles')
+    .only(['slug', 'updatedAt'])
+    .fetch()
+  const randonnees = await $content('randonnees')
+    .only(['slug', 'updatedAt'])
+    .fetch()
+  const refuges = await $content('refuges')
+    .only(['slug', 'updatedAt'])
+    .fetch()
 
   return [
-    ...articles.map(article => `/blog/${article.slug}`),
-    ...randonnees.map(randonnee => `/massifs/${randonnee.massif}/randonnees/${randonnee.slug}`),
-    ...refuges.map(refuge => `/massifs/${refuge.massif}/refuges/${refuge.slug}`)
+    ...articles.map(article => ({
+      url: 'blog/' + article.slug.replace('index', ''),
+      lastmod: Date.parse(article.updatedAt)
+    })),
+    ...randonnees.map(randonnee => ({
+      url: 'randonnees/' + randonnee.slug.replace('index', ''),
+      lastmod: Date.parse(randonnee.updatedAt)
+    })),
+    ...refuges.map(refuge => ({
+      url: 'randonnees/' + refuge.slug.replace('index', ''),
+      lastmod: Date.parse(refuge.updatedAt)
+    }))
   ]
 }
