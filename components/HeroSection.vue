64972e33c94879a412af878e44e5c39986d7ef6a
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-04-08 09:59:16 +0200

    remove useless svg

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -93,47 +93,6 @@
         <div
           class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center"
         >
-          <svg
-            class="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-8 scale-75 origin-top sm:scale-100 lg:hidden"
-            width="640"
-            height="784"
-            fill="none"
-            viewBox="0 0 640 784"
-            aria-hidden="true"
-          >
-            <defs>
-              <pattern
-                id="4f4f415c-a0e9-44c2-9601-6ded5a34a13e"
-                x="118"
-                y="0"
-                width="20"
-                height="20"
-                patternUnits="userSpaceOnUse"
-              >
-                <rect
-                  x="0"
-                  y="0"
-                  width="4"
-                  height="4"
-                  class="text-gray-200"
-                  fill="currentColor"
-                />
-              </pattern>
-            </defs>
-            <rect
-              y="72"
-              width="640"
-              height="640"
-              class="text-gray-50"
-              fill="currentColor"
-            />
-            <rect
-              x="118"
-              width="404"
-              height="784"
-              fill="url(#4f4f415c-a0e9-44c2-9601-6ded5a34a13e)"
-            />
-          </svg>
           <div class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
             <div
               class="relative block w-full bg-white rounded-lg overflow-hidden"
