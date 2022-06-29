c1432ca1b647ea0a18cc787d4d5afcf4c948dc68
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-06-29 07:29:58 +0200

    add gpx alert

--- components/trek/Overview.vue
+++ components/trek/Overview.vue
@@ -17,6 +17,11 @@
       <button
         type="button"
         class="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
+        @click="
+          alert(
+            'le fichier GPX n\'a pas encore été ajouté sur cette page. Envoyez nous un message pour le recevoir par email.'
+          )
+        "
       >
         <DownloadIcon class="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
         Télécharger GPX
@@ -36,4 +41,6 @@ const { data: geojson } = await useAsyncData(`geojson-${path}`, () => {
     .where({ _path: path, _type: 'json' })
     .findOne()
 })
+
+const alert = (msg) => window.alert(msg)
 </script>
