84ebc72b3739e8779d92bee783980e99aab7c650
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-02-08 08:30:00 +0100

    add site name on empty header on mobile

--- components/Header.vue
+++ components/Header.vue
@@ -11,6 +11,7 @@
               alt="logo mon petit sommet"
             />
           </NuxtLink>
+          <span class="ml-2 text-2xl sm:hidden font-bold text-gray-700">Mon petit sommet</span>
         </div>
         <div class="-mr-2 -my-2 md:hidden">
           <PopoverButton
